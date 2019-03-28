987fc852244233fa8e02a5f42d47c592a35bb791
Graeme Ludwig <graeme.ludwig@sessioncam.com>
2019-03-28 08:00:39 +0000

    Initial version

---
+++ src/main/java/com/example/dynamodb/CreateTable.java
@@ -0,0 +1,85 @@
+/*
+   Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+   This file is licensed under the Apache License, Version 2.0 (the "License").
+   You may not use this file except in compliance with the License. A copy of
+   the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+   This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+   specific language governing permissions and limitations under the License.
+*/
+package com.example.dynamodb;
+
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeDefinition;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.CreateTableResponse;
+import software.amazon.awssdk.services.dynamodb.model.KeySchemaElement;
+import software.amazon.awssdk.services.dynamodb.model.KeyType;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
+import software.amazon.awssdk.services.dynamodb.model.ScalarAttributeType;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Create a DynamoDB table.
+ *
+ * Takes the name of the table to create. The table will contain a single
+ * primary key, "Name".
+ *
+ */
+public class CreateTable
+{
+    public static void main(String[] args)
+    {
+        final String USAGE = "\n" +
+                "Usage:\n    CreateTable <table>\n\n" +
+                "Where:\n    table - the table to create.\n\n" +
+                "Example:\n    CreateTable HelloTable\n";
+
+        if (args.length < 1) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        /* Read the name from command args */
+        String tableName = args[0];
+
+        System.out.format(
+                "Creating table \"%s\" with a simple primary key: \"Name\".\n",
+                tableName);
+
+        CreateTableRequest request = CreateTableRequest.builder()
+                .attributeDefinitions(AttributeDefinition.builder()
+                        .attributeName("Name")
+                        .attributeType(ScalarAttributeType.S)
+                        .build())
+                .keySchema(KeySchemaElement.builder()
+                        .attributeName("Name")
+                        .keyType(KeyType.HASH)
+                        .build())
+                .provisionedThroughput(ProvisionedThroughput.builder()
+                        .readCapacityUnits(10L)
+                        .writeCapacityUnits(10L)
+                        .build())
+                .tableName(tableName)
+                .build();
+
+        try {
+            DynamoDbClient ddb = DynamoDbClient.builder().endpointOverride(new URI("http://localhost:8000")).build();
+            CreateTableResponse response = ddb.createTable(request);
+            System.out.println(response.tableDescription().tableName());
+        } catch (DynamoDbException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done!");
+    }
+}
