23293ddb2ed861f9d0a54bf796b5bebc8d51c640
Graeme Ludwig <graeme.ludwig@sessioncam.com>
2019-09-12 19:09:27 +0100

    Latest

--- dynamodbexample-v2-API/src/main/java/dynamodb/movies/Movies02LoadData.java
+++ dynamodbexample-v2-API/src/main/java/dynamodb/movies/Movies02LoadData.java
@@ -8,7 +8,9 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import static common.Utils.*;
 import static dynamodb.movies.DynamoDbUtils.*;
@@ -26,6 +28,26 @@ public class Movies02LoadData {
         ObjectNode currentNode;
         while (iterator.hasNext()) {
             currentNode = (ObjectNode) iterator.next();
+
+            String title = currentNode.path(TITLE).toString();
+            if (title.equals("\"Prisoners\"")) {
+                int rating = currentNode.path("info").path("rating").asInt();
+                String plot = currentNode.path("info").path("plot").toString();
+
+                JsonNode actorsNode = currentNode.path("info").path("actors");
+                List<String> actors = new ArrayList<>();
+                for (JsonNode node : actorsNode) {
+                    actors.add(node.asText());
+                }
+
+                System.out.println(rating);
+                System.out.println(plot);
+                for (String actor : actors) {
+                    System.out.println(actor);
+                }
+            }
+
+
             PutItemRequest request = createPutItemRequest(
                     currentNode.path(TITLE).asText(),
                     currentNode.path(YEAR).asText(),
@@ -37,3 +59,29 @@ public class Movies02LoadData {
         parser.close();
     }
 }
+
+/*
+    {
+        "year": 2013,
+        "title": "Prisoners",
+        "info": {
+            "directors": ["Denis Villeneuve"],
+            "release_date": "2013-08-30T00:00:00Z",
+            "rating": 8.2,
+            "genres": [
+                "Crime",
+                "Drama",
+                "Thriller"
+            ],
+            "image_url": "http://ia.media-imdb.com/images/M/MV5BMTg0NTIzMjQ1NV5BMl5BanBnXkFtZTcwNDc3MzM5OQ@@._V1_SX400_.jpg",
+            "plot": "When Keller Dover's daughter and her friend go missing, he takes matters into his own hands as the police pursue multiple leads and the pressure mounts. But just how far will this desperate father go to protect his family?",
+            "rank": 3,
+            "running_time_secs": 9180,
+            "actors": [
+                "Hugh Jackman",
+                "Jake Gyllenhaal",
+                "Viola Davis"
+            ]
+        }
+    },
+ */
